03fb26fdae32a87622406aab038f5f544e625d07
Tyler Cookson <tcookson0805@gmail.com>
2017-01-05 15:57:37 -0800

    console notification server is running

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var PORT = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname + '/../client')));
 // app.use("/styles", express.static(__dirname));
@@ -17,4 +18,6 @@ app.use(express.static(path.join(__dirname + '/../client')));
 //   res.sendFile(path.join(__dirname + 'views/about.html'));
 // });
 
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, function() {
+  console.log("listening on port: " + PORT)
+});
